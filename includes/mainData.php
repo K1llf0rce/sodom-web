2b733358317f809790b8df4b1f4a22507ee17af7
K1llf0rce <fteppan@protonmail.com>
2022-02-05 15:47:25 +0100

    and again

--- includes/mainData.php
+++ includes/mainData.php
@@ -4,10 +4,10 @@
     $ipVar = shell_exec("ip a | grep -m 2 -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
     $smbVar = shell_exec("systemctl is-active smbd");
     $poolVar = shell_exec("cat /tmp/webzfs.txt | grep 'state' | cut -d ' ' -f 3 | tr '[A-Z]' '[a-z]' ");
-    $poolName = shell_exec("cat /tmp/webzfs.txt | grep 'pool' | cut -d ':' -f 2 | tr -d '\r ' ");
-    $fullSpace = shell_exec('df -H /"$poolName"/ | tail -n 1 | tr -s " " | cut -d " " -f 2 | tr -d "G,T,M" ');
-    $usedSpace = shell_exec('df -H /"$poolName"/ | tail -n 1 | tr -s " " | cut -d " " -f 3 | tr -d "G,T,M" ');
-    $freeSpace = shell_exec('df -H /"$poolName"/ | tail -n 1 | tr -s " " | cut -d " " -f 4 | tr -d "G,T,M" ');
+    $poolName = shell_exec("cat /tmp/webzfs.txt | grep 'pool' | cut -d ':' -f 2 | tr -d '\r\n ' ");
+    $fullSpace = shell_exec("df -H /'$poolName'/ | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2 | tr -d 'G,T,M' ");
+    $usedSpace = shell_exec("df -H /'$poolName'/ | tail -n 1 | tr -s ' ' | cut -d ' ' -f 3 | tr -d 'G,T,M' ");
+    $freeSpace = shell_exec("df -H /'$poolName'/ | tail -n 1 | tr -s ' ' | cut -d ' ' -f 4 | tr -d 'G,T,M' ");
     $spaceDataUnit = shell_exec('df -H /"$poolName"/ | tail -n 1 | tr -s " " | cut -d " " -f 4 | tr -d "[0-9]" ');
     $poolStatus = shell_exec("cat /tmp/webzfs.txt | grep 'state' | cut -d ' ' -f 3 | tr '[A-Z]' '[a-z]' ");
     $poolMirrors = shell_exec("cat /tmp/webzfs.txt | grep -c 'mirror'");
